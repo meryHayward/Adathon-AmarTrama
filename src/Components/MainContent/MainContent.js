c10cd87a6d678944e04f17a8889719fc57c63008
Maria Hayward <hayward.maria@gmail.com>
2020-12-11 20:50:51 -0300

    formato boton

--- src/Components/MainContent/MainContent.js
+++ src/Components/MainContent/MainContent.js
@@ -2,12 +2,14 @@ import React from "react";
 import './MainContent.scss';
 import Header from "../Header/Header";
 import Search from "../Search/Search";
+import Button from "../Button/Button";
 
 const MainContent = () => {
     return (
         <React.Fragment>
             <Header />
             <Search />
+            <Button />
         </React.Fragment>
     )
 };
--- src/Components/Search/Search.js
+++ src/Components/Search/Search.js
@@ -5,9 +5,14 @@ import { faSearch } from '@fortawesome/free-solid-svg-icons'
 
 const Search = () => {
     return (
-        <section className="search-container">
-            <button className="search-container-button"><FontAwesomeIcon icon={faSearch} /></button>
-            <input className="search-container-input" placeholder="Busca tu comunidad" />
+        <section className="search">
+            <h3 className="search-title">Hola, contanos</h3>
+            <h3 className="search-title">¿De qué comunidad sos parte?</h3>
+            <div className="search-container">
+                <button className="search-container-icon"><FontAwesomeIcon icon={faSearch} /></button>
+                <input className="search-container-input" placeholder="Busca tu comunidad" />
+            </div>
+            {/*    <ButtonList /> */}
         </section>
     );
 }
